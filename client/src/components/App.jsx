c241515a59818b99e87a475f2cb0b7c0018853cd
Roman Gorelik <romang31@gmail.com>
2019-03-18 20:39:01 -0400

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+  state = {}
+
+  render () {
+    return (
+      <div>
+        Hello world
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const path = require('path')
+
+let app = express()
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+app.use(express.static(__dirname + '/client/dist/'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/dist/')
+)})
+
+app.listen(4000, () => {console.log('Listening to Port 4000!')})
