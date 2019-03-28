20214dbbb6688342c259054792a16f1163884f35
Roman Gorelik <romang31@gmail.com>
2019-03-28 12:51:25 -0400

    Fixed some server issue

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const morgan = require('morgan')
 const path = require('path')
 
 let app = express()
 app.use(bodyParser.json())
-app.use(morgan('dev'))
 app.use(express.static(__dirname + '/client/dist/'))
 
 
